7d1dc232fee53c3a4a82aad5fb399b8b1d773216
Hanish Punamiya <hanish.punamiya@sjsu.edu>
2022-11-25 21:34:56 -0800

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import User from "./Models/user.js";
 import notificationRoutes from "./Routes/notification.js";
@@ -11,12 +12,15 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"50mb"}));
 app.use(
   express.urlencoded({
+    limit:"50mb",
     extended: true,
   })
 );
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit:50000 }));
 // app.use(session({
 //     secret: process.env.TOKEN_SECRET,
 //     resave: true,

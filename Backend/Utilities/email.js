723e210d9338a15ae81b193a45e70e9ed88cc51a
Hanish Punamiya <hanish.punamiya@sjsu.edu>
2022-11-28 01:25:46 -0800

    Update email.js

--- Backend/Utilities/email.js
+++ Backend/Utilities/email.js
@@ -1,7 +1,7 @@
 import sgMail from "@sendgrid/mail";
 
 export const sendEmail = async ({ to, from, subject, text, html }) => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey("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");
   const msg = { to, from, subject, text, html };
   try {
     await sgMail.send(msg);
